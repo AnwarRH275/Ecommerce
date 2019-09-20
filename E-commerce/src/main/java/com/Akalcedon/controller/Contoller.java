e9d97360eebfbc946d2531c15975e2fac23b43cd
erhanianwar <erhanianwar@mbp-de-erhani>
2019-09-20 14:04:34 +0100

    Test git

---
+++ E-commerce/src/main/java/com/Akalcedon/controller/Contoller.java
@@ -0,0 +1,32 @@
+package com.Akalcedon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Contoller {
+	
+	
+	@RequestMapping(value = "/")
+	public String index() {
+		
+		return "index";
+	}
+	
+	@RequestMapping(value = "/category")
+	public String Categorie() {
+		return "category";
+	}
+
+	@RequestMapping(value = "/single-product")
+	public String singleproduct() {
+		return "single-product";
+	}
+	
+	@RequestMapping(value = "/checkout")
+	public String checkoutadd() {
+		return "checkout";
+	}
+	
+	
+}
